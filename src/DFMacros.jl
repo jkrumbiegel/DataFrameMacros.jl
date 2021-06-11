03064a3c2d87cb67a43816522d569e98eaefff86
Julius Krumbiegel <julius.krumbiegel@gmail.com>
2021-06-11 16:07:04 +0200

    initial commit

--- src/DFMacros.jl
+++ src/DFMacros.jl
@@ -1,5 +1,172 @@
 module DFMacros
 
-# Write your package code here.
+using Base: ident_cmp
+using DataFrames: transform, select, combine, ByRow, passmissing
+
+export @transform, @select, @combine
+
+struct Transform end
+struct Select end
+struct Combine end
+
+macro transform(df, exprs...)
+    macrohelper(Transform(), df, exprs...)
+end
+
+macro select(df, exprs...)
+    macrohelper(Select(), df, exprs...)
+end
+
+macro combine(df, exprs...)
+    macrohelper(Combine(), df, exprs...)
+end
+
+# macro groupby(df, exprs...)
+#     transform_part = macrohelper(Transform(), df, exprs...)
+#     quote
+#         temp = 
+#     end
+# end
+
+dataframesfunc(::Transform) = transform
+dataframesfunc(::Select) = select
+dataframesfunc(::Combine) = combine
+
+defaultbyrow(::Transform) = true
+defaultbyrow(::Select) = true
+defaultbyrow(::Combine) = false
+
+function macrohelper(T, df, exprs...)
+    source_func_sink_exprs = filter(is_source_func_sink_expr, exprs)
+    kw_exprs = filter(is_kw_expr, exprs)
+    converted = map(e -> convert_source_funk_sink_expr(T, e), source_func_sink_exprs)
+    converted_kw = map(convert_kw_expr, kw_exprs)
+    f = dataframesfunc(T)
+    :($f($(esc(df)), $(map(esc, converted)...); $(kw_exprs...)))
+end
+
+is_source_func_sink_expr(x) = true
+is_kw_expr(x) = false
+
+convert_source_funk_sink_expr(T, x) = x
+convert_kw_expr(x) = x
+
+function convert_source_funk_sink_expr(T, e::Expr)
+    target, formula = split_formula(e)
+    flags, formula = extract_macro_flags(formula)
+    columns = gather_columns(formula)
+    func = make_function_expr(formula, columns)
+    clean_columns = map(clean_column, columns)
+
+    byrow = (defaultbyrow(T) && !('c' in flags)) ||
+        (!defaultbyrow(T) && ('r' in flags))
+
+    pass_missing = 'm' in flags
+
+    if pass_missing
+        func = :(passmissing($func))
+    end
+
+    func = byrow ? :(ByRow($func)) : :($func)
+
+    trans_expr = if target === nothing
+        :([$(clean_columns...)] => $func)
+    else
+        :([$(clean_columns...)] => $func => $target)
+    end
+end
+
+function split_formula(e::Expr)
+    if e.head != :(=)
+        target = nothing
+        formula = e
+    else
+        target = e.args[1]
+        formula = e.args[2]
+    end
+    target, formula
+end
+
+function gather_columns(x)
+    columns = []
+    gather_columns!(columns, x)
+    columns
+end
+
+function gather_columns!(columns, x)
+    if is_column_expr(x)
+        if x ∉ columns
+            push!(columns, x)
+        end
+    elseif x isa Expr
+        foreach(x.args) do arg
+            gather_columns!(columns, arg)
+        end
+    end
+end
+
+flagchars = "crm"
+
+extract_macro_flags(x) = "", x
+
+function extract_macro_flags(e::Expr)
+    if e.head == :macrocall && all(occursin(flagchars), string(e.args[1])[2:end]) &&
+            length(e.args) == 3
+        string(e.args[1])[2:end], e.args[3] 
+    else
+        "", e
+    end
+end
+
+is_column_expr(q::QuoteNode) = true
+is_column_expr(x) = false
+function is_column_expr(e::Expr)
+    e.head == :$
+end
+
+function make_function_expr(formula, columns)
+    is_simple, symbol = is_simple_function_call(formula, columns)
+    if is_simple
+        return symbol
+    end
+
+    newsyms = map(x -> gensym(), columns)
+    replaced_formula = postwalk(formula) do e
+        i = findfirst(c -> c == e, columns)
+        if i === nothing
+            e
+        else
+            newsyms[i]
+        end
+    end
+    quote
+        ($(newsyms...),) -> $replaced_formula
+    end
+end
+
+clean_column(x) = x
+function clean_column(e::Expr)
+    if e.head == :$
+        e.args[1]
+    else
+        e
+    end
+end
+
+is_simple_function_call(x, columns) = false, nothing
+function is_simple_function_call(expr::Expr, columns)
+    is_simple = expr.head == :call &&
+        length(expr.args) >= 2 &&
+        expr.args[1] isa Symbol &&
+        all(x -> x in columns, expr.args[2:end])
+
+    is_simple, expr.args[1]
+end
+
+# from macrotools
+walk(x, inner, outer) = outer(x)
+walk(x::Expr, inner, outer) = outer(Expr(x.head, map(inner, x.args)...))
+postwalk(f, x) = walk(x, x -> postwalk(f, x), f)
+
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,82 @@
 using DFMacros
+using DataFrames
 using Test
 
-@testset "DFMacros.jl" begin
-    # Write your tests here.
+@testset "transform & select" begin
+    df = DataFrame(a = 1:100, b = rand('a':'e', 100), c = randn(100))
+
+    df2 = @transform(df, :a + 1)
+    @test df2 == transform(df, :a => ByRow(x -> x + 1))
+    df3 = @transform(df, :d = :a + 1)
+    @test df3 == transform(df, :a => ByRow(x -> x + 1) => :d)
+    df4 = @transform(df, :d = :a + 1, :e = :c * 2)
+    @test df4 == transform(df,
+        :a => ByRow(x -> x + 1) => :d,
+        :c => ByRow(x -> x * 2) => :e
+    )
+    df5 = @transform(df, :d = :a + 1, :e = :c * 2, :f = :a * :c - (:c / :a))
+    @test df5 == transform(df,
+        :a => ByRow(x -> x + 1) => :d,
+        :c => ByRow(x -> x * 2) => :e,
+        [:a, :c] => ByRow((x, y) -> x * y - (y / x)) => :f
+    )
+
+    col1 = :a
+    col2 = :c
+    df6 = @transform(df, :d = $col1 + $col2 * :a)
+    @test df6 == transform(df, [col1, col2, :a] => ByRow((x, y, z) -> x + y * z) => :d)
+
+    s = "d"
+    df7 = @transform(df, Symbol(s) = :a + 1)
+    @test df7 == transform(df, :a => ByRow(x -> x + 1) => :d)
+
+    df8 = @transform(df, AsTable = (x = "$(:a) $(:b)", y = :c * 2))
+
+    @test df8 == transform(df,
+        [:a, :b, :c] =>
+            ByRow((a, b, c) -> (x = "$a $b", y = c * 2)) => AsTable
+    )
+
+    df9 = @transform(df, :a)
+    @test df9 == transform(df, :a)
+
+    df10 = @select(df, :a * 2)
+    @test df10 == select(df, :a => ByRow(x -> x * 2))
+
+    df11 = @select(df, :x = :a * 2)
+    @test df11 == select(df, :a => ByRow(x -> x * 2) => :x)
+
+    myfunc(x) = x + 2
+    df12 = @transform(df, myfunc(:a))
+    @test "a_myfunc" in names(df12)
+    @test df12 == transform(df, :a => ByRow(myfunc))
+
+    df13 = @transform(df, :x = @c sum(:a) .+ :c)
+    @test df13 == transform(df, [:a, :c] => ((x, y) -> sum(x) .+ y) => :x)
+
+    df14 = DataFrame(a = ['a', 'b', missing])
+    df15 = @transform(df14, @m uppercase(:a))
+    # @test df15 == transform(df14, :a => ByRow(passmissing(uppercase)))
 end
+
+@testset "combine" begin
+    df = DataFrame(a = 1:100, b = rand('a':'e', 100), c = randn(100))
+
+    df1 = @combine(df, sum(:a))
+    @test df1 == combine(df, :a => sum)
+
+    df2 = @combine(df, sum(:a) + 3)
+    @test df2 == combine(df, :a => (x -> sum(x) + 3))
+
+    df3 = @combine(df, :x = sum(:a) + 3)
+    @test df3 == combine(df, :a => (x -> sum(x) + 3) => :x)
+
+    df4 = @combine(df, :y = @r 3 * :a)
+    @test df4 == combine(df, :a => ByRow(x -> 3 * x) => :y)
+end
+
+# @testset "groupby" begin
+#     df = DataFrame(a = [-1, 1, -2, 2, -3, 3], b = randn(6))
+#     gdf = @groupby(df, :c = abs(:a))
+#     @test gdf == groupby(transform(df, :a => ByRow(abs) => :c), :c)
+# end
