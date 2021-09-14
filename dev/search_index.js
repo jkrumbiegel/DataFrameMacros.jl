a1a47f4816abb2627ad54b2471e5ac535683923b
Documenter.jl <documenter@juliadocs.github.io>
2021-09-14 13:48:30 +0000

    build based on e811a4e

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#DataFrameMacros.jl","page":"DataFrameMacros.jl","title":"DataFrameMacros.jl","text":"","category":"section"},{"location":"","page":"DataFrameMacros.jl","title":"DataFrameMacros.jl","text":"Documentation for DataFrameMacros.jl","category":"page"}]
+}
